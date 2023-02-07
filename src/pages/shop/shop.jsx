e7ae0147b793b3f099a10dd1d0c4a87c9cda28f4
Justin Akridge <akridjus@gmail.com>
2023-02-06 21:16:35 -0500

    Shopping cart added

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PRODUCTS } from '../../products';
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className='shopTitle'>
+        <h1>Justin's shop</h1>
+      </div>
+      <div className='products'>
+        {" "}
+        {PRODUCTS.map((product) => (
+          <Product />
+        ))}
+      </div>
+    </div>
+  )
+}
